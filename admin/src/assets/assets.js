7174184b55be7df132fc21496d6ef794e15b003c
amb2004 <148463234+amb2004@users.noreply.github.com>
2024-07-09 23:21:22 +0530

    Added backend deployed URL

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://food-del-backend-te95.onrender.com'
 export const currency = '$'
 
 export const assets ={
