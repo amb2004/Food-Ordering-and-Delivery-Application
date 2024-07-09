1ed9446608bf3616598d8a3994095f1219745fc8
Aryan <aryanbaheti096@gmail.com>
2024-07-09 22:28:36 +0530

    first-commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,12 +8,19 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>Hi, I'm Aryan Baheti, and welcome to Tomato! Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time. At Tomato, we're not just delivering food, we're delivering happiness. Life’s too short for boring meals – let's make it a delicious adventure!</p>
             <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <img src={assets.facebook_icon} alt="Facebook" />
+              </a>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <img src={assets.twitter_icon} alt="Instagram" />
+              </a>
+              <a href="https://www.linkedin.com/in/aryan-baheti-166a17231/" target="_blank" rel="noopener noreferrer">
+                <img src={assets.linkedin_icon} alt="LinkedIn" />
+              </a>
             </div>
+
         </div>
         <div className="footer-content-center">
             <h2>COMPANY</h2>
@@ -27,8 +34,8 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+91 8234567890</li>
+                <li>aryanbaheti095@gmail.com</li>
             </ul>
         </div>
       </div>
